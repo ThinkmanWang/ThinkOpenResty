ab213465daa4424f042906a2ac90d774eac67e46
Thinkman Wang <wangxf1985@gmail.com>
2021-12-03 19:00:55 +0800

    update

--- src/controller/api_group1/hello.lua
+++ src/controller/api_group1/hello.lua
@@ -1,9 +1,7 @@
 
-local hello = require "thinkutils/test"
 local thinkutils = require "thinkutils/thinkutils"
 
 function main()
-    hello.greet("a Lua module")
     ngx.say("<p>Hello Bitch</p>")
 
     ngx.say(thinkutils.common.datetime.now())
